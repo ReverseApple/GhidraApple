e08b0f24a8902d16aa330f9b7f861a2afd66286d
Angelo D <43831545+AngeloD2022@users.noreply.github.com>
2023-12-31 17:00:46 -0500

    Improved canAnalyze inside of SelectorAliasAnalyzer

--- src/main/java/org/reverseapple/ghidraapple/analyzers/selectoralias/SelectorAliasAnalyzer.java
+++ src/main/java/org/reverseapple/ghidraapple/analyzers/selectoralias/SelectorAliasAnalyzer.java
@@ -8,8 +8,10 @@ import ghidra.app.util.importer.MessageLog;
 import ghidra.app.util.opinion.MachoLoader;
 import ghidra.program.model.address.AddressSetView;
 import ghidra.program.model.listing.Program;
+import ghidra.program.model.mem.MemoryAccessException;
 import ghidra.util.exception.CancelledException;
 import ghidra.util.task.TaskMonitor;
+import org.reverseapple.ghidraapple.utils.MachOCpuID;
 
 public class SelectorAliasAnalyzer extends AbstractAnalyzer {
 
@@ -26,7 +28,21 @@ public class SelectorAliasAnalyzer extends AbstractAnalyzer {
 
     @Override
     public boolean canAnalyze(Program program) {
-        return program.getExecutableFormat().equals(MachoLoader.MACH_O_NAME);
+        // todo: consider checking for presence of certain relevant Objective-C sections.
+
+        if (program.getExecutableFormat().equals(MachoLoader.MACH_O_NAME)) {
+            try {
+                MachOCpuID cpuArch = MachOCpuID.getCPU(program);
+
+                if (cpuArch == MachOCpuID.AARCH64 || cpuArch == MachOCpuID.AARCH64E)
+                    return true;
+
+            } catch (MemoryAccessException e) {
+                return false;
+            }
+        }
+
+        return false;
     }
 
     @Override
