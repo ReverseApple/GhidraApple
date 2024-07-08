708bb48c0fda31a70b39c79faf42c16b2b855a6d
Angelo D <43831545+AngeloD2022@users.noreply.github.com>
2024-07-08 16:12:54 -0400

    Add multiple visit methods to ClangNodeVisitor
    Populates ClangNodeVisitor with various visit methods for handling different types of nodes.

--- src/main/java/lol/fairplay/ghidraapple/core/decompiler/ClangNodeVisitor.kt
+++ src/main/java/lol/fairplay/ghidraapple/core/decompiler/ClangNodeVisitor.kt
@@ -1,9 +1,77 @@
 package lol.fairplay.ghidraapple.core.decompiler
 
-import ghidra.app.decompiler.ClangNode
+import ghidra.app.decompiler.*
 
 class ClangNodeVisitor {
     fun visit(node: ClangNode) {
         TODO()
+    }
+
+    fun visitTokenGroup(node: ClangTokenGroup) {
+
+    }
+
+    fun visitToken(node: ClangToken) {
+
+    }
+
+    fun visitVariableDecl(node: ClangVariableDecl) {
+
+    }
+
+    fun visitStatement(node: ClangStatement) {
+
+    }
+
+    fun visitFuncProto(node: ClangFuncProto) {
+
+    }
+
+    fun visitReturnType(node: ClangReturnType) {
+
+    }
+
+    fun visitFunction(node: ClangFunction) {
+
+    }
+
+    fun visitCaseToken(node: ClangCaseToken) {
+
+    }
+
+    fun visitTypeToken(node: ClangTypeToken) {
+
+    }
+
+    fun visitLabelToken(node: ClangLabelToken) {
+
+    }
+
+    fun visitCommentToken(node: ClangCommentToken) {
+
+    }
+
+    fun visitFieldToken(node: ClangFieldToken) {
+
+    }
+
+    fun visitOpToken(node: ClangOpToken) {
+
+    }
+
+    fun visitVariableToken(node: ClangVariableToken) {
+
+    }
+
+    fun visitFuncNameToken(node: ClangFuncNameToken) {
+
+    }
+
+    fun visitBreak(node: ClangBreak) {
+
+    }
+
+    fun visitSyntaxToken(node: ClangSyntaxToken) {
+
     }
 }
