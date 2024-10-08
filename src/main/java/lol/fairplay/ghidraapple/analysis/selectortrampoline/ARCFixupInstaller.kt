db6190a06f2936e05632b49c229f7129cef70c6b
Florian Magin <florian.magin@sit.fraunhofer.de>
2024-10-08 10:05:39 +0200

    Add fixups for getProperty and setProperty_atomic

--- src/main/java/lol/fairplay/ghidraapple/analysis/selectortrampoline/ARCFixupInstaller.kt
+++ src/main/java/lol/fairplay/ghidraapple/analysis/selectortrampoline/ARCFixupInstaller.kt
@@ -86,7 +86,33 @@ class ARCFixupInstallerAnalyzer() : AbstractAnalyzer(NAME, DESCRIPTION, Analyzer
             
         """.trimIndent()
 
-        val specs = listOf(retainSpec, releaseSpec, storeStrongSpec, loadSpec)
+        val getPropertySpec = """
+                        <callfixup name="_objc_getProperty">
+                            <target name="_objc_getProperty"/>
+                          <pcode>
+                            <body><![CDATA[
+                                    x0 = *(x0 + x2);
+                             ]]></body>
+                          </pcode>
+                        </callfixup>
+        """.trimIndent()
+
+        val setPropertSpec = """
+                                    <callfixup name="_objc_setProperty_atomic">
+                                        <target name="_objc_setProperty_atomic"/>
+                                      <pcode>
+                                        <body><![CDATA[
+                                                *(x0 + x3) = x2;
+                                         ]]></body>
+                                      </pcode>
+                                    </callfixup>
+        """.trimIndent()
+
+
+
+        val specs = listOf(
+            retainSpec, releaseSpec, storeStrongSpec, loadSpec, getPropertySpec, setPropertSpec,
+        )
         specs.forEach {
             specExtension.addReplaceCompilerSpecExtension(it, monitor)
         }
