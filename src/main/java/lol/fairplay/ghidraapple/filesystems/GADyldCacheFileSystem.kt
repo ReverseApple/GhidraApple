709723b2aeee980e0c606c1a6fd109ae7f20f8ef
Noah Gregory <noahmgregory@gmail.com>
2025-01-23 12:31:58 -0500

    extract out getComponentBytes function

--- src/main/java/lol/fairplay/ghidraapple/filesystems/GADyldCacheFileSystem.kt
+++ src/main/java/lol/fairplay/ghidraapple/filesystems/GADyldCacheFileSystem.kt
@@ -1,6 +1,7 @@
 package lol.fairplay.ghidraapple.filesystems
 
 import ghidra.app.util.bin.ByteProvider
+import ghidra.app.util.bin.format.macho.dyld.DyldCacheHeader
 import ghidra.file.formats.ios.dyldcache.DyldCacheFileSystem
 import ghidra.formats.gfilesystem.annotations.FileSystemInfo
 import ghidra.formats.gfilesystem.factory.GFileSystemBaseFactory
@@ -19,6 +20,7 @@ class GADyldCacheFileSystem(
     fileSystemName: String?,
     provider: ByteProvider?,
 ) : DyldCacheFileSystem(fileSystemName, provider) {
+    private var rootHeader: DyldCacheHeader? = null
     var platform: Dyld.Platform? = null
     var osVersion: Dyld.Version? = null
 
@@ -26,23 +28,22 @@ class GADyldCacheFileSystem(
         const val ROOT_HEADER_OFFSET_IN_BYTE_PROVIDER = 0 // This is defined merely for explanatory benefit.
     }
 
+    // TODO: When Ghidra 11.4 returns (or whenever the [DyldCacheHeader] getters are implemented in a release),
+    //  remove this function and replace uses of it with the implemented getters.
+    private fun getComponentBytes(componentName: String): ByteArray? {
+        val rootHeaderDataType =
+            (this.rootHeader ?: this.splitDyldCache.getDyldCacheHeader(0))
+                .toDataType() as StructureDataType? ?: return null
+        val component =
+            rootHeaderDataType.components.firstOrNull { it.fieldName == componentName } ?: return null
+        return this.provider.readBytes(
+            (ROOT_HEADER_OFFSET_IN_BYTE_PROVIDER + component.offset).toLong(),
+            component.length.toLong(),
+        )
+    }
+
     override fun open(monitor: TaskMonitor?) {
         super.open(monitor)
-        val rootHeader = this.splitDyldCache.getDyldCacheHeader(0)
-        val rootHeaderDataType = rootHeader.toDataType() as StructureDataType? ?: return
-
-        val fsByteProvider = this.provider // This is defined merely to avoid scope confusion in the below blocks.
-
-        // TODO: When Ghidra 11.4 returns (or whenever the [DyldCacheHeader] getters are implemented in a release),
-        //  remove this function and replace uses of it with the implemented getters.
-        fun getComponentBytes(componentName: String): ByteArray? {
-            val component =
-                rootHeaderDataType.components.firstOrNull { it.fieldName == componentName } ?: return null
-            return fsByteProvider.readBytes(
-                (ROOT_HEADER_OFFSET_IN_BYTE_PROVIDER + component.offset).toLong(),
-                component.length.toLong(),
-            )
-        }
 
         this.platform =
             Dyld.Platform.getPlatform(
