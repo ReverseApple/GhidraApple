bf2ea9abf7b2f5143179dbfa82365090aa6af13c
Florian Magin <florian.magin@sit.fraunhofer.de>
2025-02-03 14:49:37 +0100

    Increase analyzer resilience, add Alloc Viewer

--- src/main/java/lol/fairplay/ghidraapple/windows/AllocViewerComponent.kt
+++ src/main/java/lol/fairplay/ghidraapple/windows/AllocViewerComponent.kt
@@ -11,6 +11,7 @@ import ghidra.program.model.address.AddressSet
 import ghidra.program.model.listing.Function
 import ghidra.program.model.listing.Program
 import ghidra.program.model.symbol.Reference
+import ghidra.program.model.symbol.Symbol
 import ghidra.program.util.ProgramLocation
 import ghidra.program.util.ProgramSelection
 import ghidra.util.datastruct.Accumulator
@@ -18,67 +19,54 @@ import ghidra.util.table.GhidraFilterTable
 import ghidra.util.table.GhidraProgramTableModel
 import ghidra.util.task.TaskMonitor
 import lol.fairplay.ghidraapple.GhidraApplePluginPackage
-import lol.fairplay.ghidraapple.analysis.passes.ObjectiveCDispatchTagAnalyzer.Companion.OBJC_DISPATCH_CLASS
-import lol.fairplay.ghidraapple.analysis.passes.ObjectiveCDispatchTagAnalyzer.Companion.OBJC_DISPATCH_SELECTOR
-import lol.fairplay.ghidraapple.analysis.passes.ObjectiveCDispatchTagAnalyzer.Companion.OBJC_TRAMPOLINE
-import lol.fairplay.ghidraapple.analysis.passes.dynamicdispatch.OCSelectorAnalyzer.Companion.SELECTOR_DATA
+import lol.fairplay.ghidraapple.analysis.passes.ObjectiveCDispatchTagAnalyzer.Companion.OBJC_ALLOC
+import lol.fairplay.ghidraapple.analysis.passes.objcclasses.OCTypeInjectorAnalyzer.Companion.ALLOC_DATA
 import lol.fairplay.ghidraapple.analysis.utilities.addColumn
+import lol.fairplay.ghidraapple.analysis.utilities.address
 import lol.fairplay.ghidraapple.analysis.utilities.getFunctionsWithAnyTag
-import lol.fairplay.ghidraapple.analysis.utilities.hasTag
 import lol.fairplay.ghidraapple.plugins.ObjectiveCDynamicDispatchPlugin
 import resources.Icons
 import javax.swing.JComponent
 
-data class DynamicDispatchCallsiteData(
+data class AllocSiteData(
     val reference: Reference,
-    val calledRuntimeFunction: Function?,
-    val implementation: Function?,
-    val selector: String?,
+    val calledRuntimeFunction: Function,
+    val cls: Symbol?,
 ) {
     companion object {
         fun from(
             program: Program,
             reference: Reference,
-        ): DynamicDispatchCallsiteData {
-            val primaryRef = program.referenceManager.getPrimaryReferenceFrom(reference.fromAddress, 0)
-            val impl =
-                if (primaryRef != null && primaryRef != reference) {
-                    program.functionManager.getFunctionAt(primaryRef.toAddress)
-                } else {
-                    null
-                }
-            return DynamicDispatchCallsiteData(
-                reference,
-                program.functionManager.getFunctionAt(reference.toAddress),
-                impl,
-                program.usrPropertyManager.getStringPropertyMap(SELECTOR_DATA)?.get(reference.fromAddress),
-            )
+        ): AllocSiteData {
+            //
+            val cls: Symbol? =
+                program.usrPropertyManager
+                    .getLongPropertyMap(ALLOC_DATA)
+                    ?.get(reference.fromAddress)
+                    ?.let(program::address)
+                    ?.let(program.symbolTable::getPrimarySymbol)
+
+            return AllocSiteData(reference, program.functionManager.getFunctionAt(reference.toAddress), cls)
         }
     }
 }
 
-class DynamicDispatchComponent(
+class AllocViewerComponent(
     tool: PluginTool,
-) : ComponentProviderAdapter(tool, "Objc: Dynamic Dispatch Viewer", GhidraApplePluginPackage.PKG_NAME) {
-    private val tableModel = DynamicDispatchTable(tool)
+) : ComponentProviderAdapter(tool, "Objc: Alloc Site Viewer", GhidraApplePluginPackage.PKG_NAME) {
+//    private var refreshAction: DockingAction
+    private val tableModel = AllocTable(tool)
     private val tablePanel = GhidraFilterTable(tableModel)
 
     init {
         icon = GhidraApplePluginPackage.OBJC_ICON
     }
 
-    fun updateProgram(program: Program) {
-        tableModel.program = program
-        tableModel.reload()
-    }
-
-    fun install(tool: PluginTool): DynamicDispatchComponent {
-        tool.addComponentProvider(this, false)
+    fun install(tool: PluginTool): AllocViewerComponent {
         tablePanel.installNavigation(tool)
         val refreshAction =
             object : DockingAction("Refresh", ObjectiveCDynamicDispatchPlugin::class.simpleName) {
                 override fun actionPerformed(context: ActionContext) {
-                    tableModel.refresh()
                     tableModel.reload()
                 }
             }
@@ -86,48 +74,49 @@ class DynamicDispatchComponent(
         refreshAction.setDescription(
             "<html>Push at any time to refresh the current table of references.<br>",
         )
+        tool.addComponentProvider(this, false)
         tool.addLocalAction(this, refreshAction)
         return this
+    }
+
+    fun updateProgram(program: Program) {
+        tableModel.program = program
+        tableModel.reload()
     }
 
     override fun getComponent(): JComponent = tablePanel
 }
 
-class DynamicDispatchTable(
+class AllocTable(
     pluginTool: PluginTool,
-) : GhidraProgramTableModel<DynamicDispatchCallsiteData>("Dynamic Dispatch Table", pluginTool, null, null) {
-    override fun createTableColumnDescriptor(): TableColumnDescriptor<DynamicDispatchCallsiteData> {
-        val descriptor = TableColumnDescriptor<DynamicDispatchCallsiteData>()
+) : GhidraProgramTableModel<AllocSiteData>("Alloc Site Table", pluginTool, null, null) {
+    override fun createTableColumnDescriptor(): TableColumnDescriptor<AllocSiteData> {
+        val descriptor = TableColumnDescriptor<AllocSiteData>()
+        Address::class.java
         descriptor.addColumn("Address", true, Address::class.java) { it.reference.fromAddress }
-//        descriptor.addColumn("Receiver Type", true, String::class.java) { it }
-        descriptor.addColumn("Selector", true, String::class.java) { it.selector }
-        descriptor.addColumn("Implementation", true, Function::class.java) { it.implementation }
-        descriptor.addColumn("Is Trampoline", true, Boolean::class.java) {
-            it.calledRuntimeFunction
-                ?.tags
-                ?.map { tag -> tag.name }
-                ?.contains(OBJC_TRAMPOLINE)
-        }
+        descriptor.addColumn("Class", true, Symbol::class.java) { it.cls }
+        descriptor.addColumn(
+            "Is External Class",
+            true,
+            Boolean::class.java,
+        ) { it.cls?.address?.let(program.memory::isExternalBlockAddress) }
         descriptor.addColumn("Runtime Function", true, Function::class.java) { it.calledRuntimeFunction }
 
         return descriptor
     }
 
     override fun doLoad(
-        accumulator: Accumulator<DynamicDispatchCallsiteData>,
+        accumulator: Accumulator<AllocSiteData>,
         monitor: TaskMonitor,
     ) {
         if (program == null) return
         val data =
             program.functionManager
-                .getFunctionsWithAnyTag(OBJC_DISPATCH_SELECTOR, OBJC_DISPATCH_CLASS)
-                .asSequence()
-                .takeWhile { !monitor.isCancelled }
+                .getFunctionsWithAnyTag(OBJC_ALLOC)
                 .flatMap { program.referenceManager.getReferencesTo(it.entryPoint) }
-                .filter { it.referenceType.isCall }
-                .filterNot { program.functionManager.getFunctionContaining(it.fromAddress)?.hasTag(OBJC_TRAMPOLINE) ?: false }
-                .map { DynamicDispatchCallsiteData.from(program, it) }
-                .toSet()
+                .map { AllocSiteData.from(program, it) }
+                .sortedBy { it.reference.fromAddress }
+                .toList()
         accumulator.addAll(data)
     }
 
