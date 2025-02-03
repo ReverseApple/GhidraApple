f5cdb75fd7fdb6acadc7e9a7c6f7228287a943e0
Florian Magin <florian.magin@sit.fraunhofer.de>
2025-02-03 14:46:33 +0100

    Only enable class analysis actions at the appropriate locations

--- src/main/java/lol/fairplay/ghidraapple/plugins/ObjCInheritanceGraphPlugin.kt
+++ src/main/java/lol/fairplay/ghidraapple/plugins/ObjCInheritanceGraphPlugin.kt
@@ -9,6 +9,7 @@ import ghidra.app.services.GraphDisplayBroker
 import ghidra.framework.plugintool.PluginInfo
 import ghidra.framework.plugintool.PluginTool
 import ghidra.framework.plugintool.util.PluginStatus
+import ghidra.program.model.listing.Data
 import ghidra.util.Msg
 import ghidra.util.task.TaskLauncher
 import lol.fairplay.ghidraapple.GhidraApplePluginPackage
@@ -30,6 +31,14 @@ class ObjCInheritanceGraphPlugin(tool: PluginTool) : ProgramPlugin(tool) {
     private fun createActions() {
         val action =
             object : DockingAction("Graph class abstraction", name) {
+                override fun isEnabled(): Boolean {
+                    if (currentProgram != null && currentLocation != null) {
+                        val data: Data? = currentProgram.listing.getDefinedDataAt(currentLocation.address)
+                        return data?.dataType?.name == "class_t"
+                    }
+                    return false
+                }
+
                 override fun actionPerformed(context: ActionContext?) {
                     if (currentProgram == null) return
 
