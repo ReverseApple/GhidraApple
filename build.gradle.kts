d09d9e3835c29a639700ef10ab24b91d94ae2e1f
Noah Gregory <noahmgregory@gmail.com>
2025-02-09 17:19:20 -0500

    Make build system more Kotlin-y. (#40)
    * Make build system more Kotlin-y.
    
    * Revert Kotlin version to continue support for Jupyter.
    
    * Pin our dependencies to our Kotlin version, use non-JDK8 standard library for Kotlin.

---
+++ build.gradle.kts
@@ -0,0 +1,64 @@
+// This cannot be used inside the `plugins` block, but can (and is) used elsewhere and
+//  should match the version used inside the `plugins` block.
+val kotlinVersion = "1.9.23"
+
+plugins {
+    kotlin("jvm") version "1.9.23"
+    id("org.jlleitschuh.gradle.ktlint") version "12.1.2"
+    id("idea")
+}
+
+// Builds a Ghidra Extension for a given Ghidra installation.
+//
+// An absolute path to the Ghidra installation directory must be supplied either by setting the
+// GHIDRA_INSTALL_DIR environment variable or Gradle project property:
+//
+//     > export GHIDRA_INSTALL_DIR=<Absolute path to Ghidra>
+//     > gradle
+//
+//         or
+//
+//     > gradle -PGHIDRA_INSTALL_DIR=<Absolute path to Ghidra>
+//
+// Gradle should be invoked from the directory of the project to build.  Please see the
+// application.gradle.version property in <GHIDRA_INSTALL_DIR>/Ghidra/application.properties
+// for the correction version of Gradle to use for the Ghidra installation you specify.
+
+// ----------------------START "DO NOT MODIFY" SECTION------------------------------
+val ghidraInstallDir: String? = System.getenv("GHIDRA_INSTALL_DIR") ?: project.findProperty("GHIDRA_INSTALL_DIR") as String?
+
+if (ghidraInstallDir != null) {
+    apply(from = File(ghidraInstallDir).canonicalPath + "/support/buildExtension.gradle")
+} else {
+    throw GradleException("GHIDRA_INSTALL_DIR is not defined!")
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation(kotlin("stdlib", kotlinVersion))
+    testImplementation(kotlin("test", kotlinVersion))
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.10.3")
+    testImplementation("org.json:json:20240303")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.10.3")
+    testRuntimeOnly("org.junit.vintage:junit-vintage-engine:5.10.3")
+}
+
+kotlin {
+    jvmToolchain(21)
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+
+sourceSets {
+    main {
+        kotlin {
+            srcDirs("ghidra_scripts")
+        }
+    }
+}
+// ----------------------END "DO NOT MODIFY" SECTION-------------------------------
