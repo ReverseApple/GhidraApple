6b1c6acf3fbaab4c072bf0e269fd36e5a95cd43b
Noah Gregory <noahmgregory@gmail.com>
2025-02-09 22:38:47 -0500

    Set up initial action.

---
+++ src/main/java/lol/fairplay/ghidraapple/plugins/ObjectiveCBlockParserPlugin.kt
@@ -0,0 +1,24 @@
+package lol.fairplay.ghidraapple.plugins
+
+import ghidra.app.plugin.PluginCategoryNames
+import ghidra.app.plugin.ProgramPlugin
+import ghidra.framework.plugintool.PluginInfo
+import ghidra.framework.plugintool.PluginTool
+import ghidra.framework.plugintool.util.PluginStatus
+import lol.fairplay.ghidraapple.GhidraApplePluginPackage
+import lol.fairplay.ghidraapple.actions.setasblock.SetAsBlockAction
+
+@PluginInfo(
+    status = PluginStatus.STABLE,
+    packageName = GhidraApplePluginPackage.PKG_NAME,
+    category = PluginCategoryNames.COMMON,
+    shortDescription = "Objective-C Dynamic Dispatch",
+    description = "A plugin to help with Objective-C dynamic dispatches (msgSend family of functions)",
+)
+class ObjectiveCBlockParserPlugin(
+    tool: PluginTool,
+) : ProgramPlugin(tool) {
+    override fun init() {
+        tool.addAction(SetAsBlockAction())
+    }
+}
