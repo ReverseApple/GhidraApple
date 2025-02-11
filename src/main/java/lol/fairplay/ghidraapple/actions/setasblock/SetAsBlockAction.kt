81334474f14e1617b25bd0b7371c06e035b4901d
Noah Gregory <noahmgregory@gmail.com>
2025-02-10 21:34:40 -0500

    Add additional checks for block setting.

--- src/main/java/lol/fairplay/ghidraapple/actions/setasblock/SetAsBlockAction.kt
+++ src/main/java/lol/fairplay/ghidraapple/actions/setasblock/SetAsBlockAction.kt
@@ -4,6 +4,7 @@ import docking.ActionContext
 import docking.action.DockingAction
 import docking.action.MenuData
 import ghidra.app.context.ProgramLocationActionContext
+import ghidra.program.model.data.PointerDataType
 import lol.fairplay.ghidraapple.GhidraApplePluginPackage
 import lol.fairplay.ghidraapple.analysis.objectivec.blocks.BlockLayout
 
@@ -15,6 +16,19 @@ class SetAsBlockAction : DockingAction("Set As Objective-C Block", null) {
     override fun actionPerformed(actionContext: ActionContext?) {
         val programLocationContext =
             actionContext as? ProgramLocationActionContext ?: return
+        val dataAtLocation =
+            programLocationContext.program.listing.getDataAt(programLocationContext.address)
+                ?: throw IllegalArgumentException(
+                    "No data at address 0x${programLocationContext.address}. Please operate only on data. " +
+                        // TODO: Support stack blocks.
+                        "Stack blocks are not yet supported.",
+                )
+        if (dataAtLocation.dataType != PointerDataType.dataType) {
+            throw IllegalArgumentException(
+                "The address 0x${programLocationContext.address} does not contain a pointer. " +
+                    "This is probably not a block. Please start with an address that contains a pointer.",
+            )
+        }
         BlockLayout(programLocationContext.program, programLocationContext.address)
             .apply {
                 // TODO: Determine if we can get this to be undone with a single undo command instead of several.
