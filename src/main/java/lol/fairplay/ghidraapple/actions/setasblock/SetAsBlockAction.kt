a35986fe20c2221a7104c09c9ed7c774440b2f7e
Noah Gregory <noahmgregory@gmail.com>
2025-02-11 10:21:44 -0500

    Split out different action types.

--- src/main/java/lol/fairplay/ghidraapple/actions/setasblock/SetAsBlockAction.kt
+++ src/main/java/lol/fairplay/ghidraapple/actions/setasblock/SetAsBlockAction.kt
@@ -3,7 +3,9 @@ package lol.fairplay.ghidraapple.actions.setasblock
 import docking.ActionContext
 import docking.action.DockingAction
 import docking.action.MenuData
+import ghidra.app.context.ProgramLocationActionContext
 import ghidra.app.plugin.core.codebrowser.CodeViewerActionContext
+import ghidra.app.plugin.core.decompile.DecompilerActionContext
 import ghidra.program.model.data.Pointer
 import lol.fairplay.ghidraapple.GhidraApplePluginPackage
 import lol.fairplay.ghidraapple.analysis.objectivec.blocks.BlockLayout
@@ -15,27 +17,41 @@ class SetAsBlockAction : DockingAction("Set As Objective-C Block", null) {
 
     override fun actionPerformed(actionContext: ActionContext?) {
         val typedContext =
-            // TODO: Allow [DecompilerActionContext] for stack blocks when implementing support for them.
-            actionContext as? CodeViewerActionContext ?: return
+            actionContext as? ProgramLocationActionContext ?: return
+
+        when (typedContext) {
+            // TODO: Maybe handle cases where the address of an instruction is right clicked.
+            is CodeViewerActionContext -> handleDisassemblerLocation(typedContext)
+            is DecompilerActionContext -> handleDecompilerLocation(typedContext)
+        }
+    }
+
+    private fun handleDisassemblerLocation(context: CodeViewerActionContext) {
         val dataAtLocation =
-            typedContext.program.listing.getDataAt(typedContext.address)
+            context.program.listing.getDataAt(context.address)
                 ?: throw IllegalArgumentException(
-                    "No data at address 0x${typedContext.address}. Please operate only on data. " +
+                    "No data at address 0x${context.address}. Please operate only on data. " +
                         // TODO: Support stack blocks.
                         "Stack blocks are not yet supported.",
                 )
         if (dataAtLocation.dataType !is Pointer) {
             throw IllegalArgumentException(
-                "The address 0x${typedContext.address} does not contain a pointer. " +
+                "The address 0x${context.address} does not contain a pointer. " +
                     "This is probably not a block. Please start with an address that contains a pointer.",
             )
         }
-        BlockLayout(typedContext.program, typedContext.address)
+
+        BlockLayout(context.program, context.address)
             .apply {
                 // TODO: Determine if we can get this to be undone with a single undo command instead of several.
-                typedContext.program.withTransaction<Exception>("update program") { updateProgram() }
+                context.program.withTransaction<Exception>("update program") { updateProgram() }
             }
     }
 
-    override fun isEnabledForContext(context: ActionContext?): Boolean = context is CodeViewerActionContext
+    private fun handleDecompilerLocation(context: DecompilerActionContext): Unit =
+        throw IllegalArgumentException(
+            "Stack blocks are not support yet. Please perform this action from the Listing panel.",
+        )
+
+    override fun isEnabledForContext(context: ActionContext?): Boolean = context is ProgramLocationActionContext
 }
