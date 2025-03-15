5674168e05ca1747fa732ec58d4b40d51fc04dca
Noah Gregory <noahmgregory@gmail.com>
2025-03-15 15:53:14 -0400

    Hide pointer searching behind option.

--- src/main/java/lol/fairplay/ghidraapple/analysis/passes/blocks/ObjectiveCGlobalBlockAnalyzer.kt
+++ src/main/java/lol/fairplay/ghidraapple/analysis/passes/blocks/ObjectiveCGlobalBlockAnalyzer.kt
@@ -4,6 +4,8 @@ import ghidra.app.services.AbstractAnalyzer
 import ghidra.app.services.AnalysisPriority
 import ghidra.app.services.AnalyzerType
 import ghidra.app.util.importer.MessageLog
+import ghidra.framework.options.OptionType
+import ghidra.framework.options.Options
 import ghidra.program.model.address.AddressSetView
 import ghidra.program.model.listing.Program
 import ghidra.program.model.symbol.RefType
@@ -16,11 +18,30 @@ class ObjectiveCGlobalBlockAnalyzer : AbstractAnalyzer(NAME, DESCRIPTION, Analyz
     companion object {
         const val NAME = "Objective-C Blocks: Global Blocks"
         private const val DESCRIPTION = "Analyzes the program for Objective-C global blocks."
+
+        private const val CONST_SECTION_ITERATE_OPTION_NAME = "Iterate over `__const` sections for pointers"
+        private const val CONST_SECTION_ITERATE_OPTION_DESCRIPTION =
+            "Whether or not to iterate the `__const` sections of the main binary to look for pointers to the " +
+                "global block symbol that all global blocks start with. This is often unnecessary, but can be " +
+                "useful in cases where some such pointers are not known to Ghidra's reference manager."
     }
 
     init {
         priority = AnalysisPriority.DATA_TYPE_PROPOGATION
         setSupportsOneTimeAnalysis()
+    }
+
+    override fun registerOptions(
+        options: Options,
+        program: Program,
+    ) {
+        options.registerOption(
+            CONST_SECTION_ITERATE_OPTION_NAME,
+            OptionType.BOOLEAN_TYPE,
+            false,
+            null,
+            CONST_SECTION_ITERATE_OPTION_DESCRIPTION,
+        )
     }
 
     override fun canAnalyze(program: Program): Boolean =
@@ -37,6 +58,11 @@ class ObjectiveCGlobalBlockAnalyzer : AbstractAnalyzer(NAME, DESCRIPTION, Analyz
         monitor: TaskMonitor,
         log: MessageLog,
     ): Boolean {
+        val shouldIterateOverConstSections =
+            program
+                .getOptions(Program.ANALYSIS_PROPERTIES)
+                .getOptions(ObjectiveCGlobalBlockAnalyzer.NAME)
+                .getBoolean(CONST_SECTION_ITERATE_OPTION_NAME, false)
         program
             .let { it.getReferencesToSymbol("__NSConcreteGlobalBlock") + it.getReferencesToSymbol("__NSGlobalBlock__") }
             .filter { set.contains(it.fromAddress) }
@@ -45,10 +71,16 @@ class ObjectiveCGlobalBlockAnalyzer : AbstractAnalyzer(NAME, DESCRIPTION, Analyz
             .map { it.fromAddress }
             .toSet()
             .let { referenceAddresses ->
-                // TODO: This might be expensive in some binaries. Should we hide it behind an option?
-                FindGlobalBlockSymbolPointers(program).let {
-                    it.run(TaskMonitor.DUMMY)
-                    referenceAddresses + it.addresses
+                // TODO: We should rename this option if the behavior of [FindGlobalBlockSymbolPointers] ever changes.
+                if (shouldIterateOverConstSections) {
+                    FindGlobalBlockSymbolPointers(program).let {
+                        it.run(TaskMonitor.DUMMY)
+                        // Return the combined addresses.
+                        referenceAddresses + it.addresses
+                    }
+                } else {
+                    // Otherwise, return them as-is.
+                    referenceAddresses
                 }
             }.forEach {
                 MarkNSConcreteGlobalBlock(it).applyTo(program)
